49bc591932e51dcae953cb464c4dc00c50944d5c
irsanwoy <irsanwoy54@gmail.com>
2024-12-12 16:05:09 +0700

    menambahkan logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img src="{{ asset('images/logo.png') }}" alt="Logo" class="block h-28 w-auto">
+                        <img src="{{ asset('images/logo2.png') }}" alt="Logo" class="block h-28 w-auto">
                 </div>
 
                 <!-- Navigation Links -->
